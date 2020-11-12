582f2a5cad52259b4349532fad83c4043d46726b
GZ-Y <848552442@qq.com>
2020-11-13 00:01:16 +0800

    解决问题

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-popup>内容</van-popup>
+    <van-checkbox>复选框</van-checkbox>
+    <van-switch v-model="checked" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App',
+  // components:{
+  //   [Button.name]: Button
+  // }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/plugins/vant.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import {
+  Button,
+  Popup,
+  Checkbox,
+  CheckboxGroup,
+  Switch 
+} from 'vant';
+
+Vue.use(Button)
+.use(Popup)
+.use(Checkbox).use(CheckboxGroup)
+.use(Switch)
